a8ff653ece6f36f67918c6831ce7a786e99aa5f7
Orangel Jose <goranjose16@gmail.com>
2023-01-15 11:56:17 -0400

    Add features

--- app/posts/LikeButton.tsx
+++ app/posts/LikeButton.tsx
@@ -4,10 +4,12 @@ import { useState } from "react"
 export function LikeButton({ id }: { id: any }) {
   const [liked, setLiked] = useState(false)
   return (
-    <button className="w-auto" onClick={() => setLiked(!liked)}>
-      <p className="text-[20px]">
-        {liked ? "❤️" : "🖤"}
-      </p>
-    </button>
+    <div className="w-[100%] flex justify-center mb-[10px]">
+      <button className="w-[30px] h-[30px] bg-white flex justify-center items-center rounded-[100%]" onClick={() => setLiked(!liked)}>
+        <p className="text-[20px]">
+          {liked ? "❤️" : "🖤"}
+        </p>
+      </button>
+    </div>
   )
 }
--- app/posts/ListOfPosts.tsx
+++ app/posts/ListOfPosts.tsx
@@ -7,11 +7,15 @@ const fetchPosts = () => {
 export async function ListOfPosts() {
   const posts: any = await fetchPosts();
 
-  return posts.slice(0, 15).map((post: any) => (
-        <article className="max-w-[75ch] mb-[20px]" key={post.id}>
+  return (
+    posts.slice(0, 15).map((post: any) => (
+      <article className="max-w-[75ch] mb-[20px]" key={post.id}>
+        <div className="border-solid border-white border-[2px] mb-[10px]">
           <h2 className="text-[20px] text-[#09f]">{post.title}</h2>
-          <p className="">{post.body}</p>
-          <LikeButton id={post.id} />
-        </article>
-      ))
+          <p className="mb-[10px]">{post.body}</p>
+        </div>
+        <LikeButton id={post.id} />
+      </article>
+    ))
+  )
 }
